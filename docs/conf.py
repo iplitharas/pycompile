3898665fe0e6978278a8e6dd0ce9fd3abb340f68
iplitharas <johnplitharas@gmail.com>
2023-06-27 13:54:12 +0300

    setup furo theme for docs

--- docs/conf.py
+++ docs/conf.py
@@ -26,5 +26,5 @@ autoclass_content = "both"
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "furo"
 html_static_path = ["_static"]
